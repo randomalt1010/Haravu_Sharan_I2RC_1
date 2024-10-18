901e421bad1e45e4d3b177d46b98555f2fa09048
randomalt1010 <sharan.haravu@gmail.com>
2024-10-17 17:53:43 -0700

    f

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,40 +4,64 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class Drivetrain extends SubsystemBase {
+  private static final int LeftDriveTalonPort = 0;
+  private static final int RightDriveTalonPort = 0;
+  private final WPI_TalonSRX leftDriveTalon;
+  private final WPI_TalonSRX rightDriveTalon;
+  private final AHRS navx = new AHRS(SPI.Port.kMXP);
+  /**private ShuffleboardTab DTTab = Shuffleboard.getTab("Drivetrain");
+  private GenericEntry LeftVoltage = DTTab.add("Left Voltage", 0.0).getEntry();
+  private GenericEntry RightVoltage = DTTab.add("Right Voltage", 0.0).getEntry();**/
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public Drivetrain() {
+    leftDriveTalon = new WPI_TalonSRX(LeftDriveTalonPort);
+    leftDriveTalon.setSensorPhase(true);
+    rightDriveTalon = new WPI_TalonSRX(RightDriveTalonPort);
+    rightDriveTalon.setSensorPhase(true);
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+    leftDriveTalon.configFactoryDefault();
+    leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+    rightDriveTalon.configFactoryDefault();
+    rightDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+
+    leftDriveTalon.setNeutralMode(NeutralMode.Coast);
+    rightDriveTalon.setNeutralMode(NeutralMode.Coast);
+    rightDriveTalon.setInverted(true);
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+  public void tankDrive(double leftSpeed, double rightSpeed){
+    leftDriveTalon.set(leftSpeed);
+    rightDriveTalon.set(rightSpeed);
+  }
+
+  public double getAngle(){
+    return navx.getAngle();
+  }
+
+  public void reset(){
+    navx.reset();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Left Voltage", leftDriveTalon.getMotorOutputPercent());
+    SmartDashboard.putNumber("Right Voltage", rightDriveTalon.getMotorOutputPercent());
+    SmartDashboard.putNumber("Angle", navx.getAngle());
   }
 
   @Override
